2bc1654e94df7734bea667d4f96888303892c2c9
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-20 20:18:45 +0200

    testing the pie chart function in main (for main excists for test purposes)

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import scrape
 from scrape import *
 
 
 num = 0
 while (num < 1):
 
-    scrape("https://www.vg.no")
+    vg = scrape("https://www.vg.no", "article", 3, 4)
+    printPieChart(countCategories(vg))
     print("Hello World!")
     num += 1
