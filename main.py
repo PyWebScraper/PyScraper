9753e6d0d2e3bb592b5cd556e0dd5a99cf89457c
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-20 20:28:41 +0200

    housekeeping

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ while (num < 1):
 
     vg = scrape("https://www.vg.no", "article", 3, 4)
     printPieChart(countCategories(vg))
-    printPieChart(countSubCategories(vg))
     print("Hello World!")
     num += 1
