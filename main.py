0c406d9496d041b0d60c3661288b47e3e2b420b5
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-21 16:25:11 +0200

    pep8

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ num = 0
 while (num < 1):
 
     vg = scrape("https://www.vg.no", "article", 3, 4)
-    printPieChart(countCategories(vg))
+    print_pie_chart(count_categories(vg))
     print("Hello World!")
     num += 1
--- scrape.py
+++ scrape.py
@@ -3,6 +3,9 @@ from Article import *
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as mpl
 
+#TODO: summary of comparison to PDF
+#TODO: add extraction of date/time published
+#TODO: add extraction of date/time updated
 
 def scrape(url, articleClass, int_Where_In_URL_is_Category, int_Where_in_URL_is_SubCategory, meta_data_class=None):
     # HTTP Get request to the given URL
@@ -75,14 +78,14 @@ def themeify(url,int_Where_In_URL_is_Category, int_Where_in_URL_is_SubCategory,)
     sub_category = parts[int_Where_in_URL_is_SubCategory]
     return category, sub_category
 
-def countPhrases(list_of_article_objects, phraseToCount):
+def count_phrases(list_of_article_objects, phraseToCount):
     hits = []
     for article in range(list_of_article_objects):
         if phraseToCount in list_of_article_objects.text:
             hits.append(article)
     return len(hits)
 
-def countWord(list_of_article_objects, wordToCount):
+def count_word(list_of_article_objects, wordToCount):
 
     hits = []
     for article in range(list_of_article_objects):
@@ -90,29 +93,29 @@ def countWord(list_of_article_objects, wordToCount):
             hits.append(article)
     return len(hits)
 
-def findArticlesWithPhrase(list_of_article_objects, phraseToFind):
+def find_articles_with_phrase(list_of_article_objects, phraseToFind):
     hits = []
     for article in range(list_of_article_objects):
         if phraseToFind in list_of_article_objects.text:
             hits.append(article)
-    return prettyPrint(hits)
+    return pretty_print(hits)
 
-def findArticlesWithWord(list_of_article_objects, wordToFind):
+def find_articles_with_word(list_of_article_objects, wordToFind):
 
     hits = []
     for article in range(list_of_article_objects):
         if wordToFind in list_of_article_objects.text:
             hits.append(article)
-    return prettyPrint(hits)
+    return pretty_print(hits)
 
 
-def compareNewsWebsistes(list_of_article_objects, list_of_article_objects_2nd_website, word=None, phrase=None,
-                         url_website_1=None, url_website_2=None,
-                         metadata_class_url_one=None, metadata_class_url_two=None):
+def compare_news_websistes(list_of_article_objects, list_of_article_objects_2nd_website, word=None, phrase=None,
+                           url_website_1=None, url_website_2=None,
+                           metadata_class_url_one=None, metadata_class_url_two=None):
 
     if phrase is not None:
-        website_1_phrase = countPhrases(list_of_article_objects, phrase)
-        website_2_phrase = countPhrases(list_of_article_objects_2nd_website, phrase)
+        website_1_phrase = count_phrases(list_of_article_objects, phrase)
+        website_2_phrase = count_phrases(list_of_article_objects_2nd_website, phrase)
         if website_1_phrase > website_2_phrase:
             print(f"The first website has more occurences of the phrase: {phrase} with {website_1_phrase} occurences vs. the other websites: {website_2_phrase}\n")
         elif website_2_phrase > website_1_phrase:
@@ -121,8 +124,8 @@ def compareNewsWebsistes(list_of_article_objects, list_of_article_objects_2nd_we
             print(f"Both websites have the same number of occurances of {phrase}, with {website_1_phrase} occurences each\n")
 
     if word is not None:
-        website_1_word = countPhrases(list_of_article_objects, word)
-        website_2_word = countPhrases(list_of_article_objects_2nd_website, word)
+        website_1_word = count_phrases(list_of_article_objects, word)
+        website_2_word = count_phrases(list_of_article_objects_2nd_website, word)
         if website_1_word > website_2_word:
             print(f"The first website has more occurences of the word: {word} with {website_1_word} occurences vs. the other websites: {website_2_word}\n")
         elif website_2_word > website_1_word:
@@ -132,8 +135,8 @@ def compareNewsWebsistes(list_of_article_objects, list_of_article_objects_2nd_we
 
     if url_website_1 and url_website_2 is not None:
         if metadata_class_url_one and metadata_class_url_two is not None:
-            website_1_metaData = countMetadata(url_website_1, metadata_class_url_one)
-            website_2_metaData = countMetadata(url_website_2, metadata_class_url_two)
+            website_1_metaData = count_metadata(url_website_1, metadata_class_url_one)
+            website_2_metaData = count_metadata(url_website_2, metadata_class_url_two)
             if website_1_metaData > website_2_metaData:
                 print(
                     f"The first website has more occurences of metadata with {website_1_metaData} occurences vs. the other websites: {website_2_metaData}\n")
@@ -145,11 +148,11 @@ def compareNewsWebsistes(list_of_article_objects, list_of_article_objects_2nd_we
                     f"Both websites have the same number of occurances of metadata, with {website_1_metaData} occurences each\n")
 
     print("The two websites have the following spread of categories: ")
-    print(f"The 1st website: {countCategories(list_of_article_objects)}")
-    print(f"The 2nd website: {countCategories(list_of_article_objects_2nd_website)}")
+    print(f"The 1st website: {count_categories(list_of_article_objects)}")
+    print(f"The 2nd website: {count_categories(list_of_article_objects_2nd_website)}")
 
 
-def countCategories(list_of_article_objects):
+def count_categories(list_of_article_objects):
 
     article_counts = {}
     for article in list_of_article_objects:
@@ -163,7 +166,7 @@ def countCategories(list_of_article_objects):
     return article_counts
 
 
-def countSubCategories(list_of_article_objects):
+def count_sub_categories(list_of_article_objects):
 
     article_counts = {}
     for article in list_of_article_objects:
@@ -177,34 +180,34 @@ def countSubCategories(list_of_article_objects):
     return article_counts
 
 
-def compareHTMLCode(url1, url2):
+def compare_html_code(url1, url2):
     #TODO: find diffrences
     #TODO: the function itself
     pass
 
-def countMetadata(webpage, metadata_class):
+def count_metadata(webpage, metadata_class):
     #TODO: add logic of the function
     pass
 
 
-def compareMetadata(page1, page2, metadata_class_website_one, metadata_class_website_two):
+def compare_metadata(page1, page2, metadata_class_website_one, metadata_class_website_two):
 
-    metaCount1 = countMetadata(page1, metadata_class_website_one)
-    metaCount2 = countMetadata(page2, metadata_class_website_two)
+    meta_count1 = count_metadata(page1, metadata_class_website_one)
+    meta_count2 = count_metadata(page2, metadata_class_website_two)
 
-    if metaCount1 > metaCount2:
+    if meta_count1 > meta_count2:
         return "Page 1 has more metadata"
-    elif metaCount2 > metaCount1:
+    elif meta_count2 > meta_count1:
         return "Page 2 has more metadata"
     else:
         return "Both pages have the same amount of metadata"
 
-def prettyPrint(list):
+def pretty_print(list):
     for item in range(list):
         print(f"{item}\n")
 
 
-def printPieChart(dictionary):
+def print_pie_chart(dictionary):
 
     labels = list(dictionary.keys())
     values = list(dictionary.values())
@@ -215,9 +218,9 @@ def printPieChart(dictionary):
     mpl.show()
 
 
-def printPieChartFromLists(listOfLabels, listOfValues):
-    labels = list(listOfLabels)
-    values = list(listOfValues)
+def print_pie_chart_from_lists(list_of_labels, list_of_values):
+    labels = list(list_of_labels)
+    values = list(list_of_values)
 
     mpl.pie(values, labels=labels, autopct='%1.1f%%')
     mpl.axis('equal')
