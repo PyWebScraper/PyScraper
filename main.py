32ac0d23a67fb6fe38aeb958e7d6f01d61bfa570
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-22 21:40:32 +0200

    Sorting function

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ while (num < 1):
 
     vg = scrape("https://www.vg.no", "article", 3, 4)
     print_pie_chart(count_categories(vg))
+
     print("Hello World!")
     num += 1
--- scrape.py
+++ scrape.py
@@ -182,6 +182,12 @@ def count_sub_categories(list_of_article_objects):
     return article_counts
 
 
+def sort_list_of_objects(data, sort_param='title', reverse=False):
+    sorted_data = sorted(data, key=lambda x: getattr(x, sort_param), reverse=reverse)
+    return sorted_data
+
+
+
 def compare_html_code(url1, url2):
     #TODO: find diffrences
     #TODO: the function itself
