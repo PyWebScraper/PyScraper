283e69319d2f1c7086f946bd6896cc7fa3ad0596
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-30 12:18:04 +0200

    restructure

--- sort.py
+++
@@ -1,5 +0,0 @@
-
-def sort_list_of_objects(data, sort_param='title', reverse=False):
-    sorted_data = sorted(data, key=lambda x: (getattr(x, sort_param), '') if getattr(x, sort_param) is not None else ('', ''), reverse=reverse)
-
-    return sorted_data
