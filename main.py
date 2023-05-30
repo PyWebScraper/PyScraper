ab4106d68e283084f8b2b586a4591f73ac25b13c
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-05-30 19:17:42 +0200

    housekeeping

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-import export
-from scrape import *
+from scrape import WebCrawler, WebScraper
+from Types import NewsSite, WebStore
 
 
 num = 0
 while (num < 1):
+
+    # Create an instance of WebCrawler and WebScraper
+    crawler = WebCrawler()
+    scraper = WebScraper()
+
+    # Create a NewsSite object for vg.no
+    vg_news_site = NewsSite("VG", "https://www.vg.no", scraper)
+    vg_news_site.max_depth = 2  # Set the maximum depth
+
+    # Create a WebStore object for komplett.no
+    komplett_web_store = WebStore("Komplett", "https://www.komplett.no", scraper)
+    komplett_web_store.max_depth = 1  # Set the maximum depth
+
+    # Define the URLs to start crawling
+    start_urls = ["https://www.vg.no", "https://www.komplett.no"]
+
+    # Crawl and scrape the websites
+    for url in start_urls:
+        if url.startswith("https://www.vg.no"):
+            crawler.crawl(url, vg_news_site.max_depth)
+            vg_news_site.scrape_articles()
+        elif url.startswith("https://www.komplett.no"):
+            crawler.crawl(url, komplett_web_store.max_depth)
+            komplett_web_store.scrape_products()
+
+    # Access the scraped data
+    print("Latest articles from VG:")
+    for article in vg_news_site.articles:
+        print(article.title)
+
+    print("\nLatest products from Komplett:")
+    for product in komplett_web_store.products:
+        print(product.title)
+
+    num += 1
+    """
     url = 'https://www.vg.no'
     field_mappings = {
         'title': {
@@ -39,4 +75,4 @@ while (num < 1):
     #pie_chart = print_pie_chart(count_categories(vg), png=True, pdf=False, filename="foo")
     #sort_list_of_objects(vg, 'title')
     #print("Hello World!")
-    num += 1
+    num += 1"""
