358a1e8313a7bb854be826aa2bc211908e014e95
1SOG1 <73762521+1SOG1@users.noreply.github.com>
2023-06-01 11:09:47 +0200

    re-implementet code from article_test.py to test_pre_types.py

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
+import scrape
 from scrape import WebCrawler, WebScraper, Scraper
-from pre_types import NewsSite, WebStore
+from pre_types import NewsSite
+
 # Create an instance of WebCrawler and WebScraper
 crawler = WebCrawler()
 scraper = WebScraper()
 
-# Create a NewsSite object for example.com
-example_news_site = NewsSite("Example News Site", "https://vg.no", scraper)
-example_news_site.max_depth = 0  # Set the maximum depth
-
-# Create a WebStore object for examplestore.com
-example_web_store = WebStore("Example Web Store", "https://examplestore.com", scraper)
-example_web_store.max_depth = 0  # Set the maximum depth
+# Create a NewsSite object for vg.no
+vg_news_site = NewsSite("VG", "https://vg.no", scraper)
+vg_news_site.max_depth = 1  # Set the maximum depth
 
 # Define the URLs to start crawling
-start_urls = ["https://vg.no", "https://examplestore.com"]
+start_urls = ["https://vg.no"]
 
-# Crawl and scrape the websites
+# Crawl and scrape the website
 for url in start_urls:
     if url.startswith("https://vg.no"):
-        crawler.crawl(url, example_news_site.max_depth)
+        crawler.crawl(url, vg_news_site.max_depth)
 
-    elif url.startswith("https://examplestore.com"):
-        crawler.crawl(url, example_web_store.max_depth)
-
-# Scrape the articles and print
-example_news_site.scrape_articles(selector=".article")
-for article in example_news_site.articles:
-    print(f"Article: {article.name}\nURL: {article.url}")
-
-# Scrape the products and print
-example_web_store.scrape_products(selector=".product")
-for product in example_web_store.products:
-    print(f"Product: {product.name}\nURL: {product.url}")
+# Scrape the articles using XPath and print
+html_content = scraper.scrape(vg_news_site.url, 'html')
+article_elements = scrape.ElementSelector.extract_elements_by_xpath(html_content, "//article")
+for article_element in article_elements:
+    article_url = Scraper.extract_attribute(article_element, "href")
+    article_name = Scraper.extract_text(article_element)
+    print(f"Article: {article_name}\nURL: {article_url}")
--- test_pre_types.py
+++ test_pre_types.py
@@ -1,3 +1,80 @@
 import pytest
 from unittest.mock import MagicMock
 from pre_types import *
+
+
+@pytest.fixture
+def scraper_mock():
+    return MagicMock()
+
+
+@pytest.mark.parametrize(
+    "class_type, attrs",
+    [
+        (WebPage, {"name": "Example Page", "url": "https://example.com", "html_content": "<html>...</html>",
+                   "attr1": "value1", "attr2": "value2"}),
+        (NewsSite, {"name": "Example News Site", "url": "https://example.com", "scraper": scraper_mock,
+                    "attr1": "value1", "attr2": "value2"}),
+        (Article, {"name": "Example Article", "url": "https://example.com/article", "scraper": scraper_mock,
+                   "attr1": "value1", "attr2": "value2", "html_content": "<html>...</html>"}),
+        (WebStore, {"name": "Example Web Store", "url": "https://example.com", "scraper": scraper_mock,
+                    "attr1": "value1", "attr2": "value2"}),
+    ]
+)
+def test_classes(class_type, attrs):
+    obj = class_type(**attrs)
+
+    for attr_name, attr_value in attrs.items():
+        assert getattr(obj, attr_name) == attr_value
+
+
+def test_web_page_pretty_print_html(scraper_mock, capsys):
+    html_content = "<html>...</html>"
+    page = WebPage(name="Example Page", url="https://example.com", html_content=html_content)
+
+    # Capture the print output for assertion
+    with capsys.disabled():
+        page.pretty_print_html(html_content)  # Modify or add assertions for the print output if needed
+
+
+def test_news_site_scrape_articles(scraper_mock):
+    selector = ".article"
+    news_site = NewsSite(name="Example News Site", url="https://example.com", scraper=scraper_mock)
+
+    # Mock the scrape method to return sample HTML content
+    scraper_mock.scrape.return_value = "<html><div class='article'>Article 1</div><div class='article'>Article 2</div></html>"
+
+    news_site.scrape_articles(selector)
+
+    # Add assertions for the scraped articles
+    assert len(news_site.articles) == 2
+    assert news_site.articles[0].name == "Article"
+    assert news_site.articles[0].url == "https://example.com"
+    assert news_site.articles[0].scraper == scraper_mock
+    # Add more assertions if needed
+
+
+def test_article_pretty_print_html(scraper_mock, capsys):
+    html_content = "<html>...</html>"
+    article = Article(name="Example Article", url="https://example.com/article", scraper=scraper_mock, html_content=html_content)
+
+    # Capture the print output for assertion
+    with capsys.disabled():
+        article.pretty_print_html(html_content)  # Modify or add assertions for the print output if needed
+
+
+def test_web_store_scrape_products(scraper_mock):
+    selector = ".product-item"
+    web_store = WebStore(name="Example Web Store", url="https://example.com", scraper=scraper_mock)
+
+    # Mock the scrape method to return sample HTML content
+    scraper_mock.scrape.return_value = "<html><div class='product-item'>Product 1</div><div class='product-item'>Product 2</div></html>"
+
+    web_store.scrape_products(selector)
+
+    # Add assertions
+    assert len(web_store.products) == 2
+    assert web_store.products[0].name == "Product"
+    assert web_store.products[0].url == "https://example.com"
+    assert web_store.products[0].scraper == scraper_mock
+    # Add more assertions if needed
